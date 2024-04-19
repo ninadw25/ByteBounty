f30b92a0540b13dc499396100921aa0abb8997e3
ninadw25 <ninadwakode4@gmail.com>
2024-04-19 19:57:30 +0530

    Adding prototype login page

--- Main/Backend_final/Hackbmu/views.py
+++ Main/Backend_final/Hackbmu/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#def home(request):
+    #return HttpResponse("Hello This is ByteBounty website")
+    
 def home(request):
-    return HttpResponse("Hello This is ByteBounty website")
+        return render(request,'home.html')
