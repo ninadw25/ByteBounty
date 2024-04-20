5f3a4018c75869873d586e2360ddbab5a68fbb9c
ninadw25 <ninadwakode4@gmail.com>
2024-04-20 12:33:26 +0530

    sub commit

--- Main/Backend_final/Hackbmu/views.py
+++ Main/Backend_final/Hackbmu/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 #def home(request):
     #return HttpResponse("Hello This is ByteBounty website")
-    
+
 def home(request):
-        return render(request,'home.html')
+        return render(request,'1stpage.html')
+
+def mainpage(request):
+        return render(request,'mainpage.html')
